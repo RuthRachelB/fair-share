d23de0f7cae000b33a98586e644e025011eb43b7
ruth <rutibar839@gmail.com>
2026-06-10 22:56:48 +0300

    feat: add equal expenses splitting calculator

---
+++ fair_share.py
@@ -0,0 +1,13 @@
+def calculate_balances(expenses: dict[str, float]) -> dict[str, float]:
+    if not expenses:
+        return {}
+
+    total_expenses = sum(expenses.values())
+    num_people = len(expenses)
+    fair_share = total_expenses/num_people
+
+    balances = {}
+    for person, paid_amount in expenses.items():
+        balances[person] = round(paid_amount-fair_share, 2)
+    return balances
+
